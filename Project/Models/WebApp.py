5f512445a9017c0f2e46a40601e235200d29028e
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2020-06-18 23:39:01 +0200

    Web app creation and added files for deployment on Heroku

---
+++ Project/Models/WebApp.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 20:55:20 2020
+
+@author: Kshitij
+"""
+import sys
+sys.path.append(".")
+
+import streamlit as st
+
+from Model_LSTM_1 import LSTMModel
+
+st.title("LSTM for sentiment analysis")
+
+reviews = []
+
+lstmModel = LSTMModel()
+
+userReview = st.text_area("Enter your review here to detect emotion of it")
+st.slider("Rate your review", 0.0, 1.0, 0.5)
+#userReview = "great movie"
+
+if userReview != "":
+    print(userReview)
+    
+    reviews.append(userReview)
+    max_features = 47000
+    max_doc_len = 220
+    temp = []
+    model = lstmModel.loadModel()
+    temp, reviews = lstmModel.preProcessData(temp, reviews)
+    temp, reviews = lstmModel.encodeData(max_features, max_doc_len, temp, reviews)
+    result = lstmModel.testModel(model, reviews)
+    print(result)
+    st.write("Emotion is ", result)
+    
+
+    
