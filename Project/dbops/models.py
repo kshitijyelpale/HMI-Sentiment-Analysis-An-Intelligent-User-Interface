14dc079537e45553c2bdc6fa191613eab6ede19e
Shubham Girdhar <shubhamgirdhar94@gmail.com>
2020-08-27 14:23:21 +0200

    updating the db structure

--- Project/dbops/models.py
+++ Project/dbops/models.py
@@ -19,10 +19,19 @@ class User(db.Model):
 
 
 class Reviews(db.Model):
-    __tablename__ = "user_reviews"
+    __tablename__ = "reviews"
     id = db.Column(db.Integer, primary_key=True)
     review = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+#   user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     lstm_prediction = db.Column(db.Float)
     bayes_prediction = db.Column(db.Float)
     actual_sentiment = db.Column(db.Float)
+
+class Ratings(db.Model):
+    __tablename__ = "user_ratings"
+    id = db.Column(db.Integer, primary_key=True)
+    lstm_deviation = db.Column(db.Float)
+    bayes_deveation = db.Column(db.Float)
+    user_sentiment = db.Column(db.Float)
+    review_id = db.Column(db.Integer, db.ForeignKey("reviews.id"), nullable=False)
+
