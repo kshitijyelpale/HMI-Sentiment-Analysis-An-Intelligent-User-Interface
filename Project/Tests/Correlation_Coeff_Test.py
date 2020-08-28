f9cff886c1bc0045502821cbe5692c7953024a3a
Safir Mohammad Mustak Shaikh <safirmd47@gmail.com>
2020-08-28 23:30:44 +0200

    Correlation Test

---
+++ Project/Tests/Correlation_Coeff_Test.py
@@ -0,0 +1,50 @@
+# import libraries
+import sys
+from scipy.stats import pearsonr
+sys.path.append('../')
+from dbops.models import Reviews, db
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sentiment_analysis'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+# define class
+class CorrelationCoeff:
+
+    def __init__(self):
+        self.__rat_lst = [], []
+        self.__lstm_rat, self.__bayes_rat = [], []
+
+    # Method for importing dataset
+    def importData(self):
+        # Import data
+        self.__rat_lst = Reviews.query.with_entities(Reviews.actual_sentiment)
+        self.__lstm_rat = Reviews.query.with_entities(Reviews.lstm_prediction)
+        self.__bayes_rat = Reviews.query.with_entities(Reviews.bayes_prediction)        
+        
+    def performCorrelation(self):
+        lstm_r = pearsonr(self.__rat_lst, self.__lstm_rat)
+        bayes_r = pearsonr(self.__rat_lst, self.__bayes_rat)
+        return lstm_r, bayes_r
+
+    def printAll(self):
+        print(len(self.__lstm_rat), len(self.__bayes_rat))
+
+
+def main():
+    print("Loading Data... Please wait!")
+    r_obj = CorrelationCoeff()
+    r_obj.importData()
+    r_obj.printAll()
+    print(r_obj.performCorrelation())
+
+if __name__ == "__main__":
+    main()
