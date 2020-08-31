fd5ae8da5f5be1b40862426c5a25decb2ad87cc2
Safir Mohammad Mustak Shaikh <safirmd47@gmail.com>
2020-08-31 11:41:07 +0200

    Updated Bi_LSTM Results

--- Project/Correlation_Coeff_Test.py
+++ Project/Correlation_Coeff_Test.py
@@ -11,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sentiment_analysis'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sentiment_analysis1'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
