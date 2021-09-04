0ad43f5135dd29b8afc2197b074552c0bd8073d9
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2021-09-04 12:23:50 +0200

    React app setup with login page

---
+++ Project/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LoginPage from './components/Login/LoginPage';
+import { Button } from 'react-bootstrap';
+
+import 'react-bootstrap/dist/react-bootstrap.min';
+
+function App() {
+  return (
+    <div>
+      <Button>TEst button</Button>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ Project/client/src/components/Login/LoginPage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const LoginPage = () => {
+  return (
+    <>
+      <Container>
+        <h1 className='shadow-sm text-info mt-5 p-3 text-center rounded'>
+          Login
+        </h1>
+        <Row className='mt-5'>
+          <Col
+            lg={5}
+            md={6}
+            sm={12}
+            className='p-5 m-auto shadow-sm rounded-lg'
+          >
+            <Form>
+              <Form.Group controlId='email'>
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type='email' placeholder='Enter email' />
+              </Form.Group>
+
+              <Form.Group controlId='pwd'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type='password' placeholder='Password' />
+              </Form.Group>
+
+              <Button variant='primary btn-block mt-3' type='submit'>
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default LoginPage;
